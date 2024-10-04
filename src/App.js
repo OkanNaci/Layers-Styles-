ac09dcdb15118ce899600eec82ee3b035dc61b2c
okan <okan28001@gmail.com>
2024-10-05 00:07:24 +0300

    Src file configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Button rounded outline>
+          Click Here
+        </Button>
+      </div>
+      <div>
+        <Button>Buy Now </Button>
+      </div>
+      <div>
+        <Button>Call me </Button>
+      </div>
+      <div>
+        <Button>press me </Button>
+      </div>
+      <div>
+        <Button>haha</Button>
+      </div>
     </div>
   );
 }
-
 export default App;
