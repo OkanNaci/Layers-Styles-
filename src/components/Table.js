0fba43de05d0612208acc352f355b4f5c2c032f9
okan <okan28001@gmail.com>
2024-10-19 16:26:32 +0300

    Adding reusable table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,29 @@
-function Table({ fruits }) {
-  return <div>{fruits.length}</div>;
+function Table({ fruits, config, keyFn }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+
+  const renderedRows = fruits.map((rowData) => {
+    const renderedCells = config.map((column) => {
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(rowData)}
+        </td>
+      );
+    });
+    return (
+      <tr key={keyFn(rowData)} className="border-b">
+        {renderedCells}
+      </tr>
+    );
+  });
+  return (
+    <table className="table-auto border-spacing-3">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 export default Table;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -6,7 +6,22 @@ function TablePage() {
     { name: "Banana", color: "bg-yellow-500", score: 1 },
     { name: "Lime", color: "bg-green-500", score: 4 },
   ];
+  const config = [
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+    },
+    {
+      label: "Color",
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
+    },
+    { label: "Score", render: (fruit) => fruit.score },
+  ];
 
-  return <Table fruits={fruits} />;
+  const keyFn = (fruit) => {
+    return fruit.name;
+  };
+
+  return <Table fruits={fruits} config={config} keyFn={keyFn} />;
 }
 export default TablePage;
